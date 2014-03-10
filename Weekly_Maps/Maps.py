0a948be6499ab3db5dfe9c43a6f60ebc593a8081
dzinico <dzinico@onottar639564.AGR.GC.CA>
2014-03-10 13:48:58 -0400

    Removed static directories and made them dynamic

--- Weekly_Maps/Maps.py
+++ Weekly_Maps/Maps.py
@@ -2,12 +2,9 @@
 
 import arcpy 
 import arcpy.mapping as arcm
-from arcpy import env
 import os
 from shutil import copyfile
 import datetime
-import env
-
 
 
 '''Directories'''
@@ -38,8 +35,10 @@ testDframe = arcm.ListDataFrames(mxd)[0]
 projection = "PROJCS['North_America_Lambert_Conformal_Conic',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Lambert_Conformal_Conic'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-96.0],PARAMETER['Standard_Parallel_1',20.0],PARAMETER['Standard_Parallel_2',60.0],PARAMETER['Latitude_Of_Origin',40.0],UNIT['Meter',1.0]]"
 inproj = "GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]"
 
-input = "C:\\Users\\dzinico\\Documents\\Weekly Map Automator\\Weekly_Maps\\Input_Vectors\\GlobAv_SMUDP2_OPER_551_2013_Week_48WGS84.tif"
-output = "C:\\Users\\dzinico\Documents\\Weekly Map Automator\\Weekly_Maps\\Input_Vectors\\temp.tif"
+tiffInput = os.path.join(img_dir, str(os.listdir(img_dir)[0]))
+tiffOutput = os.path.join(vector_dir, "temp.tif")
+
+copyfile(tiffInput, tiffOutput)
 
 englishMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 frenchMonth = ['janvier', 'f�vrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'ao�t', 'septembre', 'octobre', 'novembre', 'd�cembre']
@@ -48,47 +47,39 @@ frenchMonth = ['janvier', 'f�vrier', 'mars', 'avril', 'mai', 'juin', 'juillet'
 
 
 '''Reproject tiff'''
-#if os.path.exists(output):
-#    os.remove(output)
+#if os.path.exists(tiffOutput):
+#    os.remove(tiffOutput)
 #
-reprojTif = filter(lambda x: str(x).endswith(".tif"), arcm.ListLayers(mxd))[0]
-fname = str(reprojTif)
+#reprojTif = filter(lambda x: str(x).endswith(".tif"), arcm.ListLayers(mxd))[0]
+fname = str(in_img)
 #
-#arcpy.ProjectRaster_management(input, output, projection, "", "28000", "NAD_1983_To_WGS_1984_1", "", inproj)
+#arcpy.ProjectRaster_management(tiffInput, tiffOutput, projection, "", "28000", "NAD_1983_To_WGS_1984_1", "", inproj)
 #
-#os.remove(input)
-#os.rename(output, input)
+#os.remove(tiffInput)
+#os.rename(tiffOutput, tiffInput)
 
 
 
 
 '''Set data paths'''
-#for lyr in arcm.ListLayers(mxd):
-#    name = str(lyr)
-#    print lyr.dataSource
-#    arcm.RemoveLayer(testDframe, lyr)
-#    if ((lyr.dataSource).endswith(".tif")):
-#        lyr.replaceDataSource(vector_dir, "RASTER_WORKSPACE", name)
-#    else:
-#        lyr.replaceDataSource(vector_dir, "SHAPEFILE_WORKSPACE", name)
-#    arcm.AddLayer(testDframe, lyr, "AUTO_ARRANGE")
-#    print lyr.dataSource
-#    
-#    
-#    
-#    
-#
-#mxd.save()
-#
-#
-#arcpy.mapping.ExportToPDF(mxd, r"C:\Users\dzinico\Documents\Weekly Map Automator\Weekly_Maps\Output\Map.pdf")
-
+for lyr in arcm.ListLayers(mxd):
+    name = str(lyr)
+    print lyr.dataSource
+    arcm.RemoveLayer(testDframe, lyr)
+    if ((lyr.dataSource).endswith(".tif")):
+        lyr.replaceDataSource(vector_dir, "RASTER_WORKSPACE", name)
+    else:
+        lyr.replaceDataSource(vector_dir, "SHAPEFILE_WORKSPACE", name)
+    arcm.AddLayer(testDframe, lyr, "AUTO_ARRANGE")
+    print lyr.dataSource
+    
+    
+    
+    
 
 
 
 '''Change the week number and date'''
-
-
 def iso_year_start(iso_year):
     "The gregorian calendar date of the first day of the given ISO year"
     fourth_jan = datetime.date(iso_year, 1, 4)
@@ -113,6 +104,8 @@ print weekNum
 
 for txt in arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT"):
     if txt.name == "Week Info":
-        txt.text = "Week {0} ({1} {2} - {1} {3}), {4} / Sesmaine {0} ({5} {6} au {7} {6}), {4}".format(weekNum, englishMonth[monthNum - 1], dayNum, dayNum + 6, yearNum, frenchMonth[monthNum - 1], dayNum, dayNum + 6)
+        txt.text = "Week {0} ({1} {2} - {1} {3}), {4} / Sesmaine {0} ({5} {6} au {5} {7}), {4}".format(weekNum, englishMonth[monthNum - 1], dayNum, dayNum + 6, yearNum, frenchMonth[monthNum - 1], dayNum, dayNum + 6)
 
 mxd.save()
+print os.path.join(out_dir, "Map.pdf")
+arcpy.mapping.ExportToPDF(mxd, os.path.join(out_dir, "Map.pdf"))
