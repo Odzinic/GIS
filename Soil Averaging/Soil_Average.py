767ae5f4f63602789830b5775737d8096526e965
dzinico <dzinico@onottar639564.AGR.GC.CA>
2014-04-23 13:38:42 -0400

    Commented

--- Soil Averaging/Soil_Average.py
+++ Soil Averaging/Soil_Average.py
@@ -1,63 +1,93 @@
+'''
+Created by Omar Dzinic (Omar.Dzinic@agr.gc.ca)
+April 23, 2014
+
+'''
+
+
+'''Imports'''
 import Tkinter, tkFileDialog
 import arcpy
 import os
 import xlwt
 
-arcpy.env.overwriteOutput = True
+arcpy.env.overwriteOutput = True                                                        # Enables overwriting
 arcpy.CheckOutExtension('Spatial')                                                      # Verifies that the Spatial Analyst extension is activated
 root = Tkinter.Tk()
 
 '''Directories'''
 main_dir = os.getcwd()
-#tiff_dir = tkFileDialog.askdirectory(parent=root, initialdir="/",
-#                                    title='Folder containing GeoTIFFS')
-#poly_dir = tkFileDialog.askdirectory(parent=root, initialdir="/",
-#                                    title='Folder containing polygon')
+tiff_dir = tkFileDialog.askdirectory(parent=root, initialdir="/",                       # Select directory prompt for GeoTIFF folder
+                                    title='Folder containing GeoTIFFS')
+poly_dir = tkFileDialog.askdirectory(parent=root, initialdir="/",                       # Select directory prompt for polygon folder
+                                    title='Folder containing polygon')
 
-tiff_dir = os.path.join(main_dir, 'tiff')
-poly_dir = os.path.join(main_dir, 'poly')
+#tiff_dir = os.path.join(main_dir, 'tiff')
+#poly_dir = os.path.join(main_dir, 'poly')
 
 if not os.path.exists(os.path.join(main_dir, "output")):                                # Check to see if output folder exists and creates one if not
     os.makedirs(os.path.join(main_dir, "output"))
-out_dir = os.path.join(main_dir, "output")
+out_dir = os.path.join(main_dir, "output")                                              # Path for output directory
 
 
 '''Constants'''
-polyList = filter(lambda x: x.endswith('shp'), os.listdir(poly_dir))
-tiffList = os.listdir(tiff_dir)
-poly  = os.path.join(main_dir, poly_dir, polyList[0])
+polyList = filter(lambda x: x.endswith('shp'), os.listdir(poly_dir))                    # Filters out all non .shp files in polygon folder
+tiffList = os.listdir(tiff_dir)                                                         # Path for the folder of GeoTIFFS
+poly  = os.path.join(main_dir, poly_dir, polyList[0])                                   # Path for input polygon
 
-excel = os.path.join(out_dir, "Results.csv")
+excel = os.path.join(out_dir, "Results.csv")                                            # Output path for results
 book = xlwt.Workbook()                                                                  # Creates an Excel workbook
-sh = book.add_sheet("Sheet")  
-bold = xlwt.easyxf('font: bold True;')
+sh = book.add_sheet("Sheet")                                                            # Sheet for Excel workbook
+bold = xlwt.easyxf('font: bold True;')                                                  # Font for the headers
 
-allResults = []
+allResults = []                                                                         # List that contains the results of the zonal statistic
   
 i = 1
 q = 0
 
-for tiff in tiffList:
-    tiffPath = os.path.join(tiff_dir, tiff)
-    outPath = os.path.join(out_dir, "result{0}.dbf".format(i))
-    table = arcpy.sa.ZonalStatisticsAsTable(poly, 'FID', tiffPath, outPath, 'DATA', 'MEAN')
-    sh.write(0, i, tiff, bold)
-    i += 1
 
-tempout = filter(lambda x: x.endswith('.dbf'), os.listdir(out_dir))
+'''Run Zonal Statistic on all TIFFs'''
+# Performs a Zonal Statistic As Table mean operation 
+# on every tiff in the tiff folder and saves it as a 
+# .dbf. Also writes a header at the top of the excel
+# for every tiff
 
-for res in tempout:
-    resultPath = os.path.join(out_dir, res)
-    fieldCursor = arcpy.SearchCursor(resultPath)
+# NOTE: The Zonal Statistic As Table operation skips over
+# areas with NoData values and will result in a loss of 
+# result for those polygons
+
+for tiff in tiffList:                                                                   # Iterates through all of the tiffs in the tiff folder
+    tiffPath = os.path.join(tiff_dir, tiff)                                             # Creates a path for every tiff in the iteration
+    outPath = os.path.join(out_dir, "result{0}.dbf".format(i))                          # Creates a path for the output .dbfs
+    table = arcpy.sa.ZonalStatisticsAsTable(poly, 'FID',                                # Runs Zonal Statistic As Table operation for input polygon
+                                            tiffPath, outPath, 'DATA', 'MEAN')          # and tiff
     
-    meanField = arcpy.ListFields(resultPath, "MEAN")[0]
-    allResults.append([])
+    sh.write(0, i, tiff, bold)                                                          # Writes a header for the current tiff in the iteration    
+    i += 1                                                                              # Constant used for the horizontal position of the header
+
+tempout = filter(lambda x: x.endswith('.dbf'), os.listdir(out_dir))                     # Filters the output folder to remove any non-dbf files
+
+
+'''Extract mean information from output tables'''
+# Iterates through all of the .dbf tables and extracts
+# the values from the mean field then appends it to a
+# list (allResults)
+
+for res in tempout:                                                                     # Iterates through all of the .dbf files in the output folder                                                          
+    resultPath = os.path.join(out_dir, res)                                             # Creates a path for every dbf in the iteration
+    fieldCursor = arcpy.SearchCursor(resultPath)                                        # Creates a SearchCursor for each dbf to allow for field extraction
     
-    for row in fieldCursor:
-        allResults[q].append(row.getValue(meanField.name))
+    meanField = arcpy.ListFields(resultPath, "MEAN")[0]                                 # Extracts the mean field from the dbf table
+    allResults.append([])                                                               
+    
+    for row in fieldCursor:                                                             # Iterates through all of the rows in the mean field
+        allResults[q].append(row.getValue(meanField.name))                              # Appends the values at each row in the mean field to
+                                                                                        # the list that contains the results (allResults)
     
     q += 1
-        
+
+
+'''Creates headers for polygon FIDs'''        
 q = 1
 for n in range(len(allResults[0])):
     sh.write(q, 0, n, bold)
@@ -72,4 +102,4 @@ for values in allResults:
     q = 1
     
 
-book.save(excel) 
+book.save(excel)                                                                        # Saves the Excel workbook as a .csv file
