74d51d4d22444cde8b5fd9b967a95f2ac6ac377b
dzinico <dzinico@onottar639564.AGR.GC.CA>
2014-11-03 15:54:20 -0500

    - added end of season day and ndvi - added season duration - added season amplitude - added season integrated sum - added seasonal greeness - added way of determining index of sos and eos

--- Image Interpreter/src/ndviProcessor.py
+++ Image Interpreter/src/ndviProcessor.py
@@ -9,10 +9,6 @@ import os
 import time
 import datetime
 import math
-import types
-import mpfit
-
-
 
 from scipy.optimize import curve_fit
 from scipy.interpolate import interp1d
@@ -63,26 +59,18 @@ for i, img in enumerate(lisImages):
 #     # Max NDVI !! Will need to loop through series !!
     imgStack[:, :, i] = arcpy.RasterToNumPyArray(os.path.join(imageDir, img), arcpy.Point(rasterExtentX, rasterExtentY),
                                          "", "", -9999)
-#     tmp = 0
-#    
-#     for x in range(imageWidth):
-#         for y in range(imageHeight):
-#             if (imgStack[x, y, i]  != tmp):
-#                 print imgStack[x, y, i]
-#                 tmp = imgStack[x, y, i]
-#imgStack[imgStack<0] = 0
-imgStack[:, :][imgStack[:, :]< 0] = 3000
+
+imgStack[:, :][imgStack[:, :]< 0] = 0
 endTime = time.time()
 print "Took {0} to load images".format(endTime - startTime)
-#maxNDVI = arcpy.NumPyArrayToRaster(np.amax(imgStack, 2)).save(os.path.join(outDir, "maxNDVI.tif"))
-# maxNDVI =  np.amax(imgStack[:, :, i])
-# print maxNDVI
+
 
 date = np.array(date)
 maxDay = 0
 maxNDVI = 0
 sosNDVI = 0
 sosDay = 0
+
 ''' SoS '''
 
 def func(x, *p):
@@ -93,32 +81,43 @@ def findX(y, *p):
     A, mu, sigma = p
     return (-1 * math.sqrt((-1 * math.log(y/A)) * (2 * sigma**2))) + mu
 
+def findIndex(lst, val):
+    return min(enumerate(lst), key=lambda x: abs(x[1]-val))[0]
 
 popt = np.empty((3))
 p0 = [1., 70., 10.]
-
-
 newX = np.linspace(date[0], date[-1], (date[-1] - date[0]) + 1)
-
 
 
 for x in range(imageHeight):
     for y in range(imageWidth):
         lai = imgStack[x, y]
-        currMax = np.max(lai, 0)
+        filtY = np.where(lai == 0)[0]
+        lai = np.delete(lai, filtY, 0)
+        newdate = np.delete(date, filtY, 0)
+        
         try:
-            popt, pcov = curve_fit(func, date, lai, p0)
+            popt, pcov = curve_fit(func, newdate, lai, p0)
             yFit = func(newX, *popt)
             maxDay = popt[1]
             maxNDVI = popt[0]
-            sosNDVI = yFit[0] + ((maxNDVI - yFit[0]) * 0.20)
+            sosNDVI = yFit[0] + ((maxNDVI - yFit[0]) * 0.40)
             sosDay = findX(sosNDVI, *popt)
-            print maxDay, maxNDVI, yFit[0], sosNDVI, sosDay
-        except RuntimeError:
-            #print "Max is at {0}".format(findX[popt[]])
-            pass
+            sosIndex = findIndex(yFit, sosNDVI)
+            eosNDVI = yFit[-1] + ((maxNDVI - yFit[-1]) * 0.40)
+            revFit = yFit[::-1][:20]                                                            # Check if last 20 days can actually work
+            eosIndex = (len(yFit) - 1) - findIndex(revFit, eosNDVI)
+            eosDay = newX[eosIndex]
+            seasDur = eosDay - sosDay
+            seasAmp = maxNDVI - sosNDVI
+            seasInteg = np.trapz(yFit[sosIndex:eosIndex], newX[sosIndex:eosIndex])
+            seasPASG = sum(yFit[sosIndex:eosIndex] - sosNDVI) / 10000
+            print sosIndex, eosIndex, seasInteg/10000, seasPASG
             
-        
+        except RuntimeError:
+            pass
+
+     
         
         
         
@@ -130,19 +129,27 @@ for x in range(imageHeight):
         #Plot the figure
         plt.figure()
         #plt.plot(date, peval(lai, m.params), label='Fit')
-        plt.plot(date, lai, 'r-',ls='--', marker = 'D', label="Interpolated")
-#        plt.plot(newX, newY, 'r-',ls='--', label="Cubic")
+        plt.plot(newdate, lai, 'r-',ls='--', marker = 'D', label="Interpolated")
+# #        plt.plot(newX, newY, 'r-',ls='--', label="Cubic")
         plt.plot(newX, yFit, 'g-',ls='none', marker = '*',label="EXP")
-        plt.plot(np.array(sosDay), np.array(sosNDVI), 'r-', ls = 'none', marker = 'D')
+        plt.plot(np.array(sosDay), np.array(sosNDVI), 'y-', ls = 'none', marker = 'D')
+        plt.plot(np.array(eosDay), np.array(eosNDVI), 'y-', ls = 'none', marker = 'D')
+        #plt.plot(np.array(eosDay), np.array(eosNDVI), 'y-', ls = 'none', marker = 'D')
         ##plt.plot(doy, lai,'o',newX,f(newX),'-', newX, f2(trailX),'--')
-        ##plt.legend(['data', 'linear', 'cubic'], loc='best')
+        plt.legend(['data', 'fitted data', 'start of season'], loc='best')
+        plt.xlabel("Day of year")
+        plt.ylabel("NDVI scaled up by a factor of 10000")
         plt.show()
+        #plt.savefig(r"E:\Omar\Github_Temp\Gtemp\Image Interpreter\src\figures\{0}.png".format(count), format = "png")
+#         count += 1
 #             time.sleep(1)
 #             plt.close(fig1)
 
 
 
-
+finalTime = time.time()
+print "Total time is {0}".format((finalTime - startTime) / 60)
+print finalTime      
 
 
 
