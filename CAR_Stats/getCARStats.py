ba614ca775dda3fcace3d8db20949acecff6c7f7
dzinico <dzinico@onottar639564.AGR.GC.CA>
2014-12-18 13:22:58 -0500

    -added ability to iterate through several images -fixed total pixel count -added ability to save table in output directory

--- CAR_Stats/getCARStats.py
+++ CAR_Stats/getCARStats.py
@@ -1,129 +1,110 @@
 import arcpy
 import arcpy.sa as arcsa
 import os
+from shutil import copyfile
 
 arcpy.CheckOutExtension('Spatial')
 arcpy.env.overwriteOutput = True
+arcpy.env.pyramid = "None"
 
-mainDir = os.getcwd()
-imgDir = os.path.join(mainDir, "image")
-maskDir = os.path.join(mainDir, "masks")
-carDir = os.path.join(mainDir, "car")
-tmpDir = os.path.join(mainDir, "temp")
+''' Directories '''
+mainDir = os.getcwd()                                                                                       # Root folder of script                      
+imgDir = os.path.join(mainDir, "image")                                                                     # Directory for MODIS image
+maskDir = os.path.join(mainDir, "masks")                                                                    # Directory for crop mask
+carDir = os.path.join(mainDir, "car")                                                                       # Directory for CAR shapefile
+tmpDir = os.path.join(mainDir, "temp")                                                                      # Temporary directory
+outDir = os.path.join(mainDir, "output")                                                                    # Output directory
 
-imgPath = os.path.join(imgDir, filter((lambda x: x.endswith('.tif')), os.listdir(imgDir))[0])
-maskPath = os.path.join(maskDir, filter((lambda x: x.endswith('.tif')), os.listdir(maskDir))[0])
-carPath = os.path.join(carDir, filter((lambda x: x.endswith('.shp')), os.listdir(carDir))[0])
 
-joinFields = ['COUNT', 'AREA', 'MIN', 'MAX', 'RANGE', 'MEAN', 'STD', 'SUM']
-delFields = ['ZONE_CODE', 'AREA', 'MEAN']
+'''Check if output directory exists and create one if not'''
+if (os.path.exists(outDir)):
+    next
+else:
+    os.mkdir(outDir)
+    
 
-weekNum = 30
-yearNum = 2014
+''' Path constants '''
+lisImages = filter((lambda x: x.endswith('.tif')), os.listdir(imgDir))                                      # List of  MODIS images
+maskPath = os.path.join(maskDir, filter((lambda x: x.endswith('.tif')), os.listdir(maskDir))[0])            # Path to crop mask
+carPath = os.path.join(carDir, filter((lambda x: x.endswith('.shp')), os.listdir(carDir))[0])               # Path to CAR shapefile
+timesImg = os.path.join(tmpDir, "times_img.tif") 
+projmaskPath = os.path.join(tmpDir, "project_mask.tif")                                                     # Path to projected mask
+maskedPath = os.path.join(tmpDir, "masked.tif")                                                             # Path to masked MODIS image
+statsTable = os.path.join(tmpDir, "statistics.dbf")
+totcountTable = os.path.join(tmpDir, "total_count.dbf")
 
+
+''' Projection constants '''
+# Used to project the crop mask to the user defined
+# sinusoidal projection of MODIS images
 modisProj = "PROJCS['User_Defined_Sinusoidal',GEOGCS['GCS_User_Defined',DATUM['D_User_Defined',SPHEROID['User_Defined_Spheroid',6371007.181,0.0]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Sinusoidal'],PARAMETER['false_easting',0.0],PARAMETER['false_northing',0.0],PARAMETER['central_meridian',0.0],UNIT['Meter',1.0]]"
 maskProj = "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433],METADATA['North America - NAD83',167.65,14.93,-47.74,86.45,0.0,0.0174532925199433,0.0,1350]]"
 
-arcpy.CreateCustomGeoTransformation_management("NAD83_to_MODIS_SPHERE", maskProj, modisProj, "GEOGTRAN[METHOD['Longitude_Rotation']]")
-arcpy.ProjectRaster_management(maskPath, os.path.join(tmpDir, "project_mask.tif"), imgPath, "", "", "NAD1983_TO_MODIS_SPHERE")
-print "Finished projection"
-arcsa.Times(imgPath, maskPath).save(os.path.join(tmpDir, "masked.tif"))
-print "Finished times"
-arcsa.ZonalStatisticsAsTable(carPath, 'CARUID', os.path.join(tmpDir, "masked.tif"), os.path.join(tmpDir, "table.dbf"), 'DATA', 'ALL')
+
+''' Constants '''
+joinFields = ['COUNT', 'AREA', 'MIN', 'MAX', 'RANGE', 'MEAN', 'STD', 'SUM']                                 # Fields that will be kept from Zonal
+                                                                                                            # statistics output of masked raster
+delFields = ['ZONE_CODE', 'AREA', 'MEAN']                                                                   # Fields that will be deleted from Zonal
+                                                                                                            # statistics output of crop mask
+
+dayNum = 0                                                                                                  # Week number of MODIS image
+yearNum = 0                                                                                                 # Year number of MODIS image
+
+
+''' Projecting crop mask to MODIS coordinate system '''
+ 
+arcpy.CreateCustomGeoTransformation_management("NAD83_to_MODIS_SPHERE", maskProj, modisProj, 
+                                               "GEOGTRAN[METHOD['Longitude_Rotation']]")
+arcpy.ProjectRaster_management(maskPath, projmaskPath, 
+                               os.path.join(imgDir, lisImages[0]), "", "", "NAD1983_TO_MODIS_SPHERE")
+ 
+print "Projecting completed"
+
+
+for img in lisImages:
+    imgPath = os.path.join(imgDir, img)
+    
+    ''' Applying crop mask to MODIS image '''
+    arcsa.Times(imgPath, projmaskPath).save(timesImg)                                                       # Times function to set masked out pixels as 0
+    arcsa.SetNull(timesImg, timesImg, "VALUE = 0").save(maskedPath)                                         # Sets all 0 values to NoData
+ 
+    print "Masking completed"
  
  
-#arcsa.ZonalStatisticsAsTable(carPath, 'CARUID', maskPath, os.path.join(tmpDir, "table"), 'DATA', 'ALL')
+    ''' Running zonal statistics on images '''
+    # A table named statistics.dbf is created and contains
+    # the statistics of the masked MODIS image, then another
+    # table is named total_count.dbf created containing statistics of the crop mask
+    # to allow for a comparison of pixel counts and includes
+    # areas with NoData
+    arcsa.ZonalStatisticsAsTable(carPath, 'CARUID', maskedPath, statsTable, 'DATA', 'ALL')                  # Zonal statistics of entire image      
+    arcsa.ZonalStatisticsAsTable(carPath, 'CARUID', imgPath, totcountTable, 'DATA', 'MEAN')                 # Zonal statistics to find CAR count before masking
  
-maskedRaster = arcpy.Raster(os.path.join(tmpDir, "masked.tif"))
-arcsa.Con(arcsa.IsNull(maskedRaster), 0, maskedRaster).save(os.path.join(tmpDir, "rastercalc.tif"))
-arcsa.ZonalStatisticsAsTable(carPath, 'CARUID', os.path.join(tmpDir, "project_mask.tif"), os.path.join(tmpDir, "test.dbf"), 'DATA', 'MEAN')
-arcpy.DeleteField_management(os.path.join(tmpDir, "test.dbf"), delFields)
- 
- 
-arcpy.JoinField_management(os.path.join(tmpDir, "test.dbf"), 'CARUID', os.path.join(tmpDir, "table.dbf"), 'CARUID', joinFields)
-arcpy.AddField_management(os.path.join(tmpDir, "test.dbf"), 'YEAR', 'SHORT')
-arcpy.AddField_management(os.path.join(tmpDir, "test.dbf"), 'WEEK', 'SHORT')
-arcpy.CalculateField_management(os.path.join(tmpDir, "test.dbf"), 'YEAR', yearNum, 'PYTHON')
-arcpy.CalculateField_management(os.path.join(tmpDir, "test.dbf"), 'WEEK', weekNum, 'PYTHON')
+    print "Zonal statistics completed"
+     
+
+    ''' Finding date '''
+    yearNum = int(img.split('.')[3][:4])                                                                    # Parses the name of the image using split and 
+                                                                                                            # fetches the year                                                                 
+    dayNum = int(img.split('.')[3][4:])                                                                     # Parses the name of the image using split and 
+                                                                                                            # fetches the day
 
 
+    ''' Joining tables together '''
+    arcpy.DeleteField_management(totcountTable, delFields) 
+    arcpy.JoinField_management(statsTable, 'CARUID', totcountTable, 'CARUID', joinFields)
+    
+    print "Joining tables completed"
+    
+    
+    '''Adding year and date information to the table'''
+    arcpy.AddField_management(statsTable, 'YEAR', 'SHORT')
+    arcpy.AddField_management(statsTable, 'DAY', 'SHORT')
+    arcpy.CalculateField_management(statsTable, 'YEAR', yearNum, 'PYTHON')
+    arcpy.CalculateField_management(statsTable, 'DAY', dayNum, 'PYTHON')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# try:
-#     os.mkdir(os.path.join(carDir, "split_CAR"))
-#     carSplit = os.path.join(carDir, "split_CAR")
-# except OSError:
-#     shutil.rmtree(os.path.join(carDir, "split_CAR"))
-#     os.mkdir(os.path.join(carDir, "split_CAR"))
-#     carSplit = os.path.join(carDir, "split_CAR")
-#     
-# try:
-#     os.mkdir(os.path.join(maskDir, "clip_mask"))
-#     maskClip = os.path.join(maskDir, "clip_mask")
-# except OSError:
-#     shutil.rmtree(os.path.join(maskDir, "clip_mask"))
-#     os.mkdir(os.path.join(maskDir, "clip_mask"))
-#     maskClip = os.path.join(maskDir, "clip_mask")
-#     
-# try:
-#     os.mkdir(os.path.join(imgDir, "clip_image"))
-#     imageClip = os.path.join(imgDir, "clip_image")
-# except OSError:
-#     shutil.rmtree(os.path.join(imgDir, "clip_image"))
-#     os.mkdir(os.path.join(imgDir, "clip_image"))
-#     imageClip = os.path.join(imgDir, "clip_image")
-#     
-# try:
-#     os.mkdir(os.path.join(mainDir, 'temp'))
-#     tmpDir = os.path.join(mainDir, 'temp')
-# except OSError:
-#     shutil.rmtree(os.path.join(mainDir, 'temp'))
-#     os.mkdir(os.path.join(mainDir, 'temp'))
-#     tmpDir = os.path.join(mainDir, 'temp')
-#     
-# arcpy.Split_analysis(carPath, carPath, 'CARUID', carSplit)
-# 
-# clipLst = filter((lambda x: x.endswith('.shp')), os.listdir(carSplit))
-# 
-# for clip in clipLst:
-#     imgExtract = arcsa.ExtractByMask(imgPath, os.path.join(carSplit, clip))
-#     imgExtract.save(os.path.join(imageClip, '{0}_image.tif'.format(clip)))
-#     mskExtract = arcsa.ExtractByMask(maskPath, os.path.join(carSplit, clip))
-#     imgExtract.save(os.path.join(maskClip, '{0}_mask.tif'.format(clip)))
-#     
-#     mskImage = arcsa.Times(os.path.join(imageClip, '{0}_image.tif'.format(clip)), os.path.join(maskClip, '{0}_mask.tif'.format(clip)))
-#     mskImage.save(os.path.join(tmpDir, '{0}_masked.tif'.format(clip)))
-#         
+    
+    '''Copy final table to output directory'''
+    copyfile(statsTable, os.path.join(outDir, "{0}.dbf").format(img[:len(img)-4]))
+    
+    print "Table saved at {0}".format(os.path.join(outDir, "{0}.dbf"))
