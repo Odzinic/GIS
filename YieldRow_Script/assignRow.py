ac4d00672411af28221aced3ff8a47e5fb483ca8
DZINICOM <DZINICOM@OND4C00757763.cihs.ad.gov.on.ca>
2015-05-26 15:50:25 -0400

    - made compatible with ArcMap interface

--- YieldRow_Script/assignRow.py
+++ YieldRow_Script/assignRow.py
@@ -1,52 +1,83 @@
+
+''' Imports '''
 import os
 import arcpy
-from arcpy import sa as arcs
-from arcpy import da as arcda
 import numpy as np
 arcpy.CheckOutExtension('Spatial')                                                      # Verifies that the Spatial Analyst extension is activated
 print "Imported arcpy"
 
-np.seterr(all='ignore')
+np.seterr(all='ignore')                                                                 # Suppresses numpy warnings
 
-input_dir = os.path.join(os.getcwd(), "input")
 
-inputs = os.listdir(input_dir)                                                            # List that loads all files in the mask/buffer directory
+''' Directories '''
+# input_dir = os.path.join(os.getcwd(), "input")                                          
+
+# inputs = os.listdir(input_dir)                                                            # List that loads all files in the mask/buffer directory
+points = arcpy.GetParameterAsText(0)
 # points = os.path.join(input_dir, filter((lambda x: x.endswith(".shp")), inputs)[0])       # Loads only shapefile masks/buffers to prevent useless files
-points = r"I:\Scripts\GIS\YieldRow_Script\projected\Randy Cain-Home-All-2014-Harvest-Harvest-Corn.shp"
+# points = r"I:\Scripts\GIS\YieldRow_Script\projected\Randy Cain-Home-All-2014-Harvest-Harvest-Corn.shp"
 inputName = points.split('\\')[-1]
 inputName = inputName.replace('-', '')
-tableDir = r"I:\Scripts\GIS\YieldRow_Script\table_test"
+# tableDir = r"I:\Scripts\GIS\YieldRow_Script\table_test"
+tableDir = arcpy.GetParameter(1)
 
-pointDic = {}
-currDegree = 0
-prevDegree = 50
-pathNum = 1
+
+
+''' Constants '''
+pointDic = {}                                                                           # Dictionary used to store all points from shapefile
+currDegree = 0                                                                          # Variable that saves the direction degree of current point
+prevDegree = 50                                                                         # Variable that saves the direction degree of previous point
+                                                                                        # default set to the degree threshold
+pathNum = 1                                                                             # Variable that stores the current path/load number
 pathLen = {}
 threshConst = 0.025
 pathThresh = 0
 longPaths = []
+degreeField = arcpy.GetParameterAsText(2)
+yieldField = arcpy.GetParameterAsText(3)
+upperSTD = arcpy.GetParameter(4)
+lowerSTD = arcpy.GetParameter(5)
 
 
-arcpy.MakeTableView_management(points, "myTableView")
-total = int(arcpy.GetCount_management("myTableView").getOutput(0))
-pathThresh = total * threshConst
- 
- 
-with arcpy.da.SearchCursor(points, ["FID", "Heading", "DryYield", "Path_Num"]) as cursor: # @UndefinedVariable
+# arcpy.MakeTableView_management(points, "myTableView")
+# total = int(arcpy.GetCount_management("myTableView").getOutput(0))
+# pathThresh = total * threshConst
+#  
+
+arcpy.AddField_management(points, "Path_Num", "SHORT")                                  # Add a field for the path number
+
+# SearchCursor goes through all the points in the input shapefile
+# and saves the FID(key), degree(value 1), yield(value 2) and creates a variable for path number(value 3)
+# in a dictionary 
+with arcpy.da.SearchCursor(points, ["FID", degreeField, yieldField, "Path_Num"]) as cursor: # @UndefinedVariable
     for row in cursor:
-        pointDic[row[0]] = [row[1], row[2], row[3], 0]
+        pointDic[row[0]] = [row[1], row[2], row[3], 0]                                  # Storing each tuple in dictionary with the FID being the key
  
 if (pointDic[0][0] >= 50):
     pathNum = 0    
+
+
+# Iterates through every tuple that was stored in the dictionary
+# and checks whether the degree delta is 50 or more. If true,
+# the path number(pathNum) increases by one and is assigned to
+# the tuple         
+for key in pointDic.keys():                                                             # Iterate through all of the keys in the point dictionary(pointDic)
+        row = pointDic[key]                                                             # Retrieves and stores all of the stored values for that key
+        currDegree = row[0]                                                             # Retrieves the degree value from the current tuple
          
-for key in pointDic.keys():
-        row = pointDic[key]
-        currDegree = row[0]
-         
-        if (abs(currDegree - prevDegree) >= 50):
+        if (abs(currDegree - prevDegree) >= 50):                                        # Checks to see if the degree difference is 50 or more
+            
+            
+            
+            
+            # Checks to see if the point 5 after the current point has a smaller difference
+            # than the difference between current and past to prevent spurious tuples from
+            # making the path number increase
+            # NOTE: The try/except prevents out of bound list errors by grabbing the last
+            # value in the list if an out of bound error occurs
             try:
-#                 lookahCheck = (abs((pointDic[key + 10][0] - currDegree)) < abs((pointDic[key + 10][0] - prevDegree)))
-                lookahCheck = (abs((pointDic[key + 5][0] - currDegree)) < (abs(currDegree - prevDegree)))
+                lookahCheck = (abs((pointDic[key + 5][0] - currDegree)) < 
+                               (abs(currDegree - prevDegree)))
             except KeyError:
                 lookahCheck = (abs((pointDic[pointDic.keys()[-1]][0] - currDegree)) < abs((pointDic[pointDic.keys()[-1]][0] - prevDegree)))
              
@@ -85,46 +116,26 @@ for key in pointDic.keys():
                 pathLen[pathNum] = 1
             prevDegree = currDegree
              
-# for path in longPaths:
-#     longKeys = []
-#     for row in pointDic.items():
-#         if row[1][2] == path:
-#             longKeys.append(row[0])
-#         
-#     print pointDic[longKeys[0]]    
-#     prevDegree = pointDic[longKeys[0]][0]
-#     pathNum = path
-#     for key in longKeys:
-#         currDegree = pointDic[key][0]
-#         if (abs(currDegree - prevDegree) >= 9):
-#             print "Path changed"                    
-#             pathNum += 100
-#             pointDic[key][2] = pathNum
-#              
-#             prevDegree = currDegree
-#         
-#         else:
-#             pointDic[key][2] = pathNum
-#             prevDegree = currDegree
-globYield = arcpy.da.TableToNumPyArray(points, ["DryYield"]) # @UndefinedVariable
-globMean = np.mean(globYield["DryYield"])
-globSTD = np.std(globYield["DryYield"])
-globuppThresh = globMean + (2*globSTD)
-globlowThresh = globMean - (1.2*globSTD)
+
+globYield = arcpy.da.TableToNumPyArray(points, [yieldField]) # @UndefinedVariable
+globMean = np.mean(globYield[yieldField])
+globSTD = np.std(globYield[yieldField])
+globuppThresh = globMean + (upperSTD*globSTD)
+globlowThresh = globMean - (lowerSTD*globSTD)
 print globMean, globSTD, globuppThresh, globlowThresh
  
 for path in pathLen.keys():
-    locYield = globYield = arcpy.da.TableToNumPyArray(points, ["DryYield"], '"Path_Num" = {0}'.format(path))
-    locMean = np.mean(locYield["DryYield"])
-    locSTD = np.std(locYield["DryYield"])
-    locuppThresh = locMean + (2*locSTD)
-    loclowThresh = locMean - (1.2*locSTD)
+    locYield = globYield = arcpy.da.TableToNumPyArray(points, [yieldField], '"Path_Num" = {0}'.format(path))
+    locMean = np.mean(locYield[yieldField])
+    locSTD = np.std(locYield[yieldField])
+    locuppThresh = locMean + (upperSTD*locSTD)
+    loclowThresh = locMean - (lowerSTD*locSTD)
      
     for key in filter((lambda x: pointDic[x][2] == path), pointDic.keys()):
         if (pointDic[key][2] < loclowThresh and pointDic[key][2] > locuppThresh):
             pointDic[key][3] = 1 
            
-with arcpy.da.UpdateCursor(points, ["FID", "Heading", "Elevatio", "Path_Num", "DryYield"]) as cursor: # @UndefinedVariable
+with arcpy.da.UpdateCursor(points, ["FID", degreeField, "Elevatio", "Path_Num", yieldField]) as cursor: # @UndefinedVariable
     for row in cursor:
         if (row[4] > globlowThresh and row[4] < globuppThresh and pointDic[row[0]][3] == 0):
             row[3] = pointDic[row[0]][2]
@@ -144,7 +155,7 @@ threshConst = 0.025
 pathThresh = 0
 longPaths = []
              
-with arcpy.da.SearchCursor(points, ["FID", "Heading", "DryYield", "Path_Num"]) as cursor: # @UndefinedVariable
+with arcpy.da.SearchCursor(points, ["FID", degreeField, yieldField, "Path_Num"]) as cursor: # @UndefinedVariable
     for row in cursor:
         pointDic[row[0]] = [row[1], row[2], row[3], 0]
  
@@ -197,12 +208,14 @@ for key in pointDic.keys():
                 pathLen[pathNum] = 1
             prevDegree = currDegree
              
-with arcpy.da.UpdateCursor(points, ["FID", "Heading", "Elevatio", "Path_Num", "DryYield"]) as cursor: # @UndefinedVariable
+with arcpy.da.UpdateCursor(points, ["FID", degreeField, "Elevatio", "Path_Num", yieldField ]) as cursor: # @UndefinedVariable
     for row in cursor:
         row[3] = pointDic[row[0]][2]
         cursor.updateRow(row)
 
-
+arcpy.AddXY_management(points)
+arcpy.AddField_management(points, "Obj_ID", "LONG")
+arcpy.CalculateField_management(points, "Obj_ID", "!FID!", "PYTHON")
          
 tableMapp = arcpy.FieldMappings()
 
@@ -216,10 +229,12 @@ tableFields = arcpy.FieldMap()
 tableFields.addInputField(points, "Obj_ID")
 tableMapp.addFieldMap(tableFields)
 tableFields = arcpy.FieldMap()
-tableFields.addInputField(points, "DryYield")
+tableFields.addInputField(points, yieldField)
 tableMapp.addFieldMap(tableFields)
 tableFields = arcpy.FieldMap()
 tableFields.addInputField(points, "Path_Num") 
 tableMapp.addFieldMap(tableFields)
+
+
 
 arcpy.TableToTable_conversion(points, tableDir, inputName[:inputName.index(".shp")],'',  tableMapp)
--- YieldRow_Script/rowStatistics.py
+++ YieldRow_Script/rowStatistics.py
@@ -1,18 +1,30 @@
+
+''' Imports '''
 import arcpy
 from arcpy import sa as arcsa
 import os
 
-arcpy.CheckOutExtension("Spatial")
+arcpy.CheckOutExtension("Spatial")                                                      # Verifies that the Spatial Analyst extension is activated
 
 
-inputDir = r"I:\Scripts\GIS\YieldRow_Script\Test"
-rasterDir = os.path.join(inputDir, "rasters")
-ascFiles = filter((lambda x: x.endswith(".asc") and not("sd" in x)), os.listdir(inputDir))
-convrasPaths = []
-totalRasters = len(ascFiles)
+# inputDir = r"I:\Scripts\GIS\YieldRow_Script\Test"
+# rasterDir = os.path.join(inputDir, "rasters")
+
+''' Directories '''
+inputDir = arcpy.GetParameterAsText(0)                                                  # Directory that contains the ASCII files
+rasterDir = arcpy.GetParameterAsText(1)                                                 # Directory that will contain the output rasters
+
+
+''' Constants '''
+ascFiles = filter((lambda x: x.endswith(".asc") and                                     # List of all the ASCII files in the input director
+                   not("sd" in x)), os.listdir(inputDir))                               # NOTE: Filters out ASCII files that contain "sd" in them
+
+convrasPaths = []                                                                       # List that stores the file paths to the converted ASCII rasters
+totalRasters = len(ascFiles)                                                            # Calculates the number of ASCII input files
 coordSys = "PROJCS['NAD_1983_UTM_Zone_17N',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',500000.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-81.0],PARAMETER['Scale_Factor',0.9996],PARAMETER['Latitude_Of_Origin',0.0],UNIT['Meter',1.0]]"
 finalRaster = os.path.join(rasterDir, "yi{0}year".format(totalRasters))
-os.mkdir(rasterDir)
+# os.mkdir(rasterDir)
+
 
 for ascii in ascFiles:
     inputAscii = os.path.join(inputDir, ascii)
