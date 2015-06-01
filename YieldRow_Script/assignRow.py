51f88e5d7885126ae7d81e9eba149daf21b6748d
dzinicom <dzinicom@OND4C00757763.cihs.ad.gov.on.ca>
2015-06-01 15:55:02 -0400

    - fixed issue with degrees barely passing 360 degree resulting in new path

--- YieldRow_Script/assignRow.py
+++ YieldRow_Script/assignRow.py
@@ -27,16 +27,20 @@ tableDir = arcpy.GetParameter(1)
 pointDic = {}                                                                           # Dictionary used to store all points from shapefile
 currDegree = 0                                                                          # Variable that saves the direction degree of current point
 prevDegree = 50                                                                         # Variable that saves the direction degree of previous point
+threshDegree = 70
                                                                                         # default set to the degree threshold
 pathNum = 1                                                                             # Variable that stores the current path/load number
 pathLen = {}
+lookaheadConst = 10
 threshConst = 0.025
 pathThresh = 0
 longPaths = []
 degreeField = arcpy.GetParameterAsText(2)
 yieldField = arcpy.GetParameterAsText(3)
-upperSTD = arcpy.GetParameter(4)
-lowerSTD = arcpy.GetParameter(5)
+elevField = arcpy.GetParameterAsText(4)
+upperSTD = arcpy.GetParameter(5)
+lowerSTD = arcpy.GetParameter(6)
+outputFields = ["POINT_X", "POINT_Y", "Obj_ID", yieldField, "Path_Num"]
 
 
 # arcpy.MakeTableView_management(points, "myTableView")
@@ -64,8 +68,25 @@ if (pointDic[0][0] >= 50):
 for key in pointDic.keys():                                                             # Iterate through all of the keys in the point dictionary(pointDic)
         row = pointDic[key]                                                             # Retrieves and stores all of the stored values for that key
         currDegree = row[0]                                                             # Retrieves the degree value from the current tuple
+        
+        if (abs(prevDegree - currDegree) > 200):
+            
+            if (abs(360 - abs(currDegree - prevDegree)) > threshDegree):
+   
+                pathNum += 1
+                row[2] = pathNum
+                prevDegree = currDegree
+                     
+            else:
+                row[2] = pathNum
+                if (pathLen.has_key(pathNum)):
+                    pathLen[pathNum] += 1   
+                else:
+                    pathLen[pathNum] = 1
+                prevDegree = currDegree
+                
          
-        if (abs(currDegree - prevDegree) >= 50):                                        # Checks to see if the degree difference is 50 or more
+        elif (abs(currDegree - prevDegree) >= threshDegree):                                        # Checks to see if the degree difference is 50 or more
             
             
             
@@ -76,7 +97,7 @@ for key in pointDic.keys():                                                     
             # NOTE: The try/except prevents out of bound list errors by grabbing the last
             # value in the list if an out of bound error occurs
             try:
-                lookahCheck = (abs((pointDic[key + 5][0] - currDegree)) < 
+                lookahCheck = (abs((pointDic[key + lookaheadConst][0] - currDegree)) < 
                                (abs(currDegree - prevDegree)))
             except KeyError:
                 lookahCheck = (abs((pointDic[pointDic.keys()[-1]][0] - currDegree)) < abs((pointDic[pointDic.keys()[-1]][0] - prevDegree)))
@@ -88,8 +109,8 @@ for key in pointDic.keys():                                                     
                 else:
                     pathLen[pathNum] = 1
                      
-                if (pathLen[pathNum] >= pathThresh):
-                    longPaths.append(pathNum)
+#                 if (pathLen[pathNum] >= pathThresh):
+#                     longPaths.append(pathNum)
                      
                  
                      
@@ -115,7 +136,6 @@ for key in pointDic.keys():                                                     
             else:
                 pathLen[pathNum] = 1
             prevDegree = currDegree
-             
 
 globYield = arcpy.da.TableToNumPyArray(points, [yieldField]) # @UndefinedVariable
 globMean = np.mean(globYield[yieldField])
@@ -125,7 +145,7 @@ globlowThresh = globMean - (lowerSTD*globSTD)
 print globMean, globSTD, globuppThresh, globlowThresh
  
 for path in pathLen.keys():
-    locYield = globYield = arcpy.da.TableToNumPyArray(points, [yieldField], '"Path_Num" = {0}'.format(path))
+    locYield = globYield = arcpy.da.TableToNumPyArray(points, [yieldField], '"Path_Num" = {0}'.format(path)) # @UndefinedVariable
     locMean = np.mean(locYield[yieldField])
     locSTD = np.std(locYield[yieldField])
     locuppThresh = locMean + (upperSTD*locSTD)
@@ -135,7 +155,7 @@ for path in pathLen.keys():
         if (pointDic[key][2] < loclowThresh and pointDic[key][2] > locuppThresh):
             pointDic[key][3] = 1 
            
-with arcpy.da.UpdateCursor(points, ["FID", degreeField, "Elevatio", "Path_Num", yieldField]) as cursor: # @UndefinedVariable
+with arcpy.da.UpdateCursor(points, ["FID", degreeField, elevField, "Path_Num", yieldField]) as cursor: # @UndefinedVariable
     for row in cursor:
         if (row[4] > globlowThresh and row[4] < globuppThresh and pointDic[row[0]][3] == 0):
             row[3] = pointDic[row[0]][2]
@@ -159,17 +179,43 @@ with arcpy.da.SearchCursor(points, ["FID", degreeField, yieldField, "Path_Num"])
     for row in cursor:
         pointDic[row[0]] = [row[1], row[2], row[3], 0]
  
-if (pointDic[0][0] >= 50):
+if (pointDic[0][0] >= threshDegree):
     pathNum = 0    
          
-for key in pointDic.keys():
-        row = pointDic[key]
-        currDegree = row[0]
+for key in pointDic.keys():                                                             # Iterate through all of the keys in the point dictionary(pointDic)
+        row = pointDic[key]                                                             # Retrieves and stores all of the stored values for that key
+        currDegree = row[0]                                                             # Retrieves the degree value from the current tuple
+        
+        if (abs(prevDegree - currDegree) > 200):
+            
+            if (abs(360 - abs(currDegree - prevDegree)) > threshDegree):
+   
+                pathNum += 1
+                row[2] = pathNum
+                prevDegree = currDegree
+                     
+            else:
+                row[2] = pathNum
+                if (pathLen.has_key(pathNum)):
+                    pathLen[pathNum] += 1   
+                else:
+                    pathLen[pathNum] = 1
+                prevDegree = currDegree
+                
          
-        if (abs(currDegree - prevDegree) >= 50):
+        elif (abs(currDegree - prevDegree) >= threshDegree):                                        # Checks to see if the degree difference is 50 or more
+            
+            
+            
+            
+            # Checks to see if the point 5 after the current point has a smaller difference
+            # than the difference between current and past to prevent spurious tuples from
+            # making the path number increase
+            # NOTE: The try/except prevents out of bound list errors by grabbing the last
+            # value in the list if an out of bound error occurs
             try:
-#                 lookahCheck = (abs((pointDic[key + 10][0] - currDegree)) < abs((pointDic[key + 10][0] - prevDegree)))
-                lookahCheck = (abs((pointDic[key + 5][0] - currDegree)) < (abs(currDegree - prevDegree)))
+                lookahCheck = (abs((pointDic[key + lookaheadConst][0] - currDegree)) < 
+                               (abs(currDegree - prevDegree)))
             except KeyError:
                 lookahCheck = (abs((pointDic[pointDic.keys()[-1]][0] - currDegree)) < abs((pointDic[pointDic.keys()[-1]][0] - prevDegree)))
              
@@ -180,14 +226,14 @@ for key in pointDic.keys():
                 else:
                     pathLen[pathNum] = 1
                      
-                if (pathLen[pathNum] >= pathThresh):
-                    longPaths.append(pathNum)
+#                 if (pathLen[pathNum] >= pathThresh):
+#                     longPaths.append(pathNum)
                      
                  
                      
                 pathNum += 1
                 row[2] = pathNum
-        
+                 
                   
                 prevDegree = currDegree
                  
@@ -198,7 +244,7 @@ for key in pointDic.keys():
                     pathLen[pathNum] += 1   
                 else:
                     pathLen[pathNum] = 1
-                precvDegree = currDegree
+                prevDegree = currDegree
                
         else:
             row[2] = pathNum
@@ -208,7 +254,7 @@ for key in pointDic.keys():
                 pathLen[pathNum] = 1
             prevDegree = currDegree
              
-with arcpy.da.UpdateCursor(points, ["FID", degreeField, "Elevatio", "Path_Num", yieldField ]) as cursor: # @UndefinedVariable
+with arcpy.da.UpdateCursor(points, ["FID", degreeField, elevField, "Path_Num", yieldField ]) as cursor: # @UndefinedVariable
     for row in cursor:
         row[3] = pointDic[row[0]][2]
         cursor.updateRow(row)
@@ -219,22 +265,27 @@ arcpy.CalculateField_management(points, "Obj_ID", "!FID!", "PYTHON")
          
 tableMapp = arcpy.FieldMappings()
 
-tableFields = arcpy.FieldMap()
-tableFields.addInputField(points, "POINT_X")
-tableMapp.addFieldMap(tableFields)
-tableFields = arcpy.FieldMap()
-tableFields.addInputField(points, "POINT_Y")
-tableMapp.addFieldMap(tableFields)
-tableFields = arcpy.FieldMap()
-tableFields.addInputField(points, "Obj_ID")
-tableMapp.addFieldMap(tableFields)
-tableFields = arcpy.FieldMap()
-tableFields.addInputField(points, yieldField)
-tableMapp.addFieldMap(tableFields)
-tableFields = arcpy.FieldMap()
-tableFields.addInputField(points, "Path_Num") 
-tableMapp.addFieldMap(tableFields)
+for field in outputFields:
+    tableFields = arcpy.FieldMap()
+    tableFields.addInputField(points, field)
+    tableMapp.addFieldMap(tableFields)
+    
+# tableFields = arcpy.FieldMap()
+# tableFields.addInputField(points, "POINT_X")
+# tableMapp.addFieldMap(tableFields)
+# tableFields = arcpy.FieldMap()
+# tableFields.addInputField(points, "POINT_Y")
+# tableMapp.addFieldMap(tableFields)
+# tableFields = arcpy.FieldMap()
+# tableFields.addInputField(points, "Obj_ID")
+# tableMapp.addFieldMap(tableFields)
+# tableFields = arcpy.FieldMap()
+# tableFields.addInputField(points, yieldField)
+# tableMapp.addFieldMap(tableFields)
+# tableFields = arcpy.FieldMap()
+# tableFields.addInputField(points, "Path_Num") 
+# tableMapp.addFieldMap(tableFields)
 
 
 
-arcpy.TableToTable_conversion(points, tableDir, inputName[:inputName.index(".shp")],'',  tableMapp)
+# arcpy.TableToTable_conversion(points, tableDir, inputName[:inputName.index(".shp")],'',  tableMapp)
