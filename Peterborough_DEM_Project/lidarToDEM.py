3c9a3b424e5ed28b1872950bbce3f4d40f9cb9f5
dzinicom <dzinicom@OND4C00757763.cihs.ad.gov.on.ca>
2016-01-25 16:23:00 -0500

    -added ground and non-ground classification

--- Peterborough_DEM_Project/lidarToDEM.py
+++ Peterborough_DEM_Project/lidarToDEM.py
@@ -1,5 +1,6 @@
 import arcpy, os
 from arcpy import sa as arcsa
+from macpath import join
 
 arcpy.env.overwriteOutput = True
 
@@ -13,10 +14,13 @@ lidartileIn = arcpy.GetParameterAsText(1)
 tileField = arcpy.GetParameterAsText(2)
 outDir = arcpy.GetParameterAsText(3)
 cellSize = arcpy.GetParameterAsText(4)
+comparedemIn = arcpy.GetParameterAsText(5)
 
 lasLst = []                 # List of all of the las files in index
 lasdOut = os.path.join(outDir, "tiled_LAS.lasd")
 demOut = os.path.join(outDir, "DEM.tif")
+groundpolyOut = os.path,join(outDir, "Ground_Polygon.shp")
+nongroundpolyOut = os.path,join(outDir, "Non_Ground_Polygon.shp")
 
 arcpy.MakeFeatureLayer_management(lidartileIn, "LAS_Index")
 
@@ -26,6 +30,14 @@ with arcpy.da.SearchCursor("LAS_Index", ["FID", tileField]) as cursor:          
     
 arcpy.CreateLasDataset_management(lasLst, lasdOut, "NO_RECURSION")
 arcpy.LasDatasetToRaster_conversion(lasdOut, demOut, "ELEVATION", "BINNING MINIMUM SIMPLE", "FLOAT", "CELLSIZE", cellSize, "1")
+
+arcpy.Minus_3d(demOut, comparedemIn, "in_memory\\diff")
+arcpy.gp.Con_sa("in_memory\\diff", "1", "in_memory\\ground", "", "Value < .5")
+arcpy.gp.Con_sa("in_memory\\diff", "1", "in_memory\\nonground", "", "Value > .5")
+arcpy.RasterToPolygon_conversion("in_memory\\ground", groundpolyOut, "NO_SIMPLIFY", "VALUE")
+arcpy.RasterToPolygon_conversion("in_memory\\ground", nongroundpolyOut, "NO_SIMPLIFY", "VALUE")
+
+
                            
     
 
