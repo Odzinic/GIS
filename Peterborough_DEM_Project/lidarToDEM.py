c8c7b639debedcdce79f2f23e35f3b2cf5e07964
dzinicom <dzinicom@OND4C00757763.cihs.ad.gov.on.ca>
2016-02-18 15:34:35 -0500

    - changed outputting to memory to outputting to file - fixed some syntax errors

--- Peterborough_DEM_Project/lidarToDEM.py
+++ Peterborough_DEM_Project/lidarToDEM.py
@@ -1,6 +1,5 @@
 import arcpy, os
 from arcpy import sa as arcsa
-from macpath import join
 
 arcpy.env.overwriteOutput = True
 
@@ -15,27 +14,37 @@ tileField = arcpy.GetParameterAsText(2)
 outDir = arcpy.GetParameterAsText(3)
 cellSize = arcpy.GetParameterAsText(4)
 comparedemIn = arcpy.GetParameterAsText(5)
+coordsysIn = arcpy.GetParameterAsText(6)
 
 lasLst = []                 # List of all of the las files in index
 lasdOut = os.path.join(outDir, "tiled_LAS.lasd")
 demOut = os.path.join(outDir, "DEM.tif")
-groundpolyOut = os.path,join(outDir, "Ground_Polygon.shp")
-nongroundpolyOut = os.path,join(outDir, "Non_Ground_Polygon.shp")
+groundpolyOut = os.path.join(outDir, "Ground_Polygon.shp")
+nongroundpolyOut = os.path.join(outDir, "Non_Ground_Polygon.shp")
+minusrasOut = os.path.join(outDir, "Minus_Raster.tif")
+groundrasOut = os.path.join(outDir, "Ground_Raster.tif")
+nongroundrasOut = os.path.join(outDir, "Non_Ground_Raster.tif")
 
 arcpy.MakeFeatureLayer_management(lidartileIn, "LAS_Index")
-
+ 
 with arcpy.da.SearchCursor("LAS_Index", ["FID", tileField]) as cursor:                  # @UndefinedVariable
     for row in cursor:
-        lasLst.append(os.path.join(lidarIn, row[1] + ".las"))
-    
-arcpy.CreateLasDataset_management(lasLst, lasdOut, "NO_RECURSION")
+        if (arcpy.Exists(os.path.join(lidarIn, row[1] + ".las"))):
+            lasLst.append(os.path.join(lidarIn, row[1] + ".las"))
+        else: pass
+     
+arcpy.CreateLasDataset_management(lasLst, lasdOut, "NO_RECURSION", "", coordsysIn)
 arcpy.LasDatasetToRaster_conversion(lasdOut, demOut, "ELEVATION", "BINNING MINIMUM SIMPLE", "FLOAT", "CELLSIZE", cellSize, "1")
 
-arcpy.Minus_3d(demOut, comparedemIn, "in_memory\\diff")
-arcpy.gp.Con_sa("in_memory\\diff", "1", "in_memory\\ground", "", "Value < .5")
-arcpy.gp.Con_sa("in_memory\\diff", "1", "in_memory\\nonground", "", "Value > .5")
-arcpy.RasterToPolygon_conversion("in_memory\\ground", groundpolyOut, "NO_SIMPLIFY", "VALUE")
-arcpy.RasterToPolygon_conversion("in_memory\\ground", nongroundpolyOut, "NO_SIMPLIFY", "VALUE")
+arcpy.Minus_3d(demOut, comparedemIn, minusrasOut)
+arcpy.gp.Con_sa(minusrasOut, "1", groundrasOut, "", "Value < .5")
+arcpy.RasterToPolygon_conversion(groundrasOut, groundpolyOut, "NO_SIMPLIFY", "VALUE")
+# arcpy.Delete_management("in_memory\\ground")
+
+arcpy.gp.Con_sa(minusrasOut, "1", nongroundrasOut, "", "Value > .5")
+# arcpy.Delete_management("in_memory\\diff")
+arcpy.RasterToPolygon_conversion(nongroundrasOut, nongroundpolyOut, "NO_SIMPLIFY", "VALUE")
+
 
 
                            
